fee23283962c5b3e44ffc8cf588b58e4e530dc25
Anirudh Jayakumar <anirudh.jayakumar@outlook.com>
2019-01-30 20:17:31 +0000

    MailBoxBoard

---
+++ include/mailbox.h
@@ -0,0 +1,20 @@
+#include "bitboard.h"
+#include "constants.h"
+#include "primitives.h"
+
+#pragma once
+
+class MailBox
+{
+  private:
+    Piece list[64];
+
+  public:
+    MailBox();            // Constructs blank mailbox
+    MailBox(Piece l[64]); // Constructs mailbox from a list of pieces
+    MailBox(MailBox &m);  // Constructs mailbox with contents equal to m
+
+    Piece Occupation(Square sq) const;  // Returns the piece on sq
+    void SetSquare(Square sq, Piece p); // Sets the piece on sq to p
+    BitBoard Pieces(Piece p) const;     // Debug method - can check sync with bitboards
+};
